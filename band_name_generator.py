6c9bdd32b55cfb5c072666c889c5570866b64331
SANJAY SANTHANAGOPAL <sanjay.santhanagopal@mercedes-benz.com>
2024-02-28 09:35:24 +0530

    band name generatory script

---
+++ band_name_generator.py
@@ -0,0 +1,25 @@
+# Date : 28.02.2024 - 5 am
+# string manipulation and code intelligence :
+
+# print("Hello world\nHello world\nHello world")  # new line character \
+# print("She said \"Hello\" ")  # escape character
+# print("Hello " + "Sanjay")  #
+# spacing is very important in python
+# indentation error, syntax error
+# debugging example and practice
+# # user input function :
+# print("Hello" + input("What is your name ?"))
+# naming conventions :
+# 1. meaningful names
+
+# program to generate the brand name based on the city and the user's pet
+
+city = input("Which city did you grow up ? \n")
+pet = input("what is the name of your pet ? \n")
+print("Your band name could be :" + city + " " + pet)
+
+
+
+
+
+
